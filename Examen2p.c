c1feacb48f17baf9a8b9ba167fb7e5fd1b95daee
Juan Patiño <juan.patino@udla.edu.ec>
2023-12-06 16:08:26 -0500

    Impriemdo que es lo que deso que aparezca para el final

--- Examen2p.c
+++ Examen2p.c
@@ -7,3 +7,25 @@ void mostrarMat(float calif[][23]);
 void promEst(float calif[][23]);
 void promGrupo(float calif[][23]);
 int mejorEst(float calif[][23]);
+
+int main() {
+    float calif[3][23];
+
+    genCalif(calif);
+    
+    printf("Matriz de Calificaciones:\n");
+    mostrarMat(calif);
+
+    printf("\nPromedio de cada estudiante por progreso:\n");
+    promEst(calif);
+
+    printf("\nNota promedio del grupo por progreso:\n");
+    promGrupo(calif);
+
+    int mejor = mejorEst(calif);
+    printf("\nEl estudiante con el mejor promedio global es: %d\n", mejor + 1);
+
+    return 0;
+}
+
+
